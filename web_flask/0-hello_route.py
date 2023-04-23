fd7804d69527b857f51ad610b16ff06a13d45b2b
Aymen <aymenabduseid10@gmail.com>
2023-04-23 20:15:55 +0300

    hot fix

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Hello Flask!
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
