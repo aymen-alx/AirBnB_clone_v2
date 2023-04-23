8f2dcd9e82d7082dece754fe417a4d35aa50394c
Aymen <aymenabduseid10@gmail.com>
2023-04-23 22:00:27 +0300

    hot fix

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-    Basic falsk web application
+Hello Flask!, HBNB, C is fun!,...
 """
+
 from flask import Flask, escape, render_template
 from models import storage
 from models.state import State
@@ -13,21 +14,17 @@ app.url_map.strict_slashes = False
 
 @app.route('/hbnb_filters')
 def hbnb_filters():
-    """
-        list of all State objects present
-    """
-    states = list(storage.all('State').values())
+    """ """
     amenities = list(storage.all('Amenity').values())
+    states = list(storage.all('State').values())
     return render_template('10-hbnb_filters.html', tasks=states, amenities=amenities)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """
-        After each request you must remove the current session
-    """
+    """ """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Basic falsk web application
+Hello Flask!, HBNB, C is fun!,...
 """
 from flask import Flask, escape, render_template
 from models import storage
@@ -16,30 +16,20 @@ app.url_map.strict_slashes = False
 
 @app.route('/hbnb')
 def hbnb():
-    """
-        list of all State objects present
-    """
-    states = list(storage.all('State').values())
-    amenities = list(storage.all('Amenity').values())
+    """ """
     places = list(storage.all('Place').values())
     cities = list(storage.all('City').values())
     users = list(storage.all('User').values())
-    # print('========Debuging session============')
-    # print(type(places[0]))
-    # for walla in users:
-    #     if walla.id == 'fa44780d-ac48-41ab-9dd0-ac54a15755cf':
-    #         print(walla.first_name)
-    # print('====================================')
+    states = list(storage.all('State').values())
+    amenities = list(storage.all('Amenity').values())
     return render_template('100-hbnb.html', tasks=states, amenities=amenities, places=places, users=users)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """
-        After each request you must remove the current session
-    """
+    """ """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
