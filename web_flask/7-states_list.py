9583abf8ab9e0b798a0c89ef0d224bc101c33a31
Aymen <aymenabduseid10@gmail.com>
2023-04-23 21:29:17 +0300

    hot fix

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Hello Flask!, HBNB, C is fun!,...
+"""
+
+from flask import Flask, escape, render_template
+from models import storage
+from models import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """ """
+    state = list(storage.all('State').values())
+    return render_template('7-states_list.html', states=state)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """ """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
